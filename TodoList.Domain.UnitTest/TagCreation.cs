ab24ad7b8d79e3b9862919ce0e44ee21be0397b1
nathan <nathan.esslinger67@gmail.com>
2023-12-10 15:37:26 +0100

    UnitTest TagCreation/Update

--- TodoList.Domain.UnitTest/TagCreation.cs
+++ TodoList.Domain.UnitTest/TagCreation.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TodoList.Domain.Entities;
+using TodoList.Domain.ValueObjects;
 
 namespace TodoList.Domain.UnitTest
 {
@@ -22,9 +23,79 @@ namespace TodoList.Domain.UnitTest
       Assert.AreEqual(tag.Name, name);
       Assert.IsNotNull(tag.Id);
       Assert.IsNull(tag.Description);
-      Assert.AreEqual(tag.Color, "#000000");
+      Assert.AreEqual(tag.Color, new Color("#000000"));
       Assert.IsNull(tag.TagParent);
-      Assert.IsTrue(tag.AssociatedTask.Count == 0);
+    }
+
+    [TestMethod]
+    [DataRow("Tag 1", "Description 1")]
+    public void Constructor_ShouldCreateTag_WithDescription(string name, string description)
+    {
+      Tag tag = new Tag.TagBuilder()
+          .SetName(name)
+          .SetDescription(description)
+          .Build();
+
+      Assert.AreEqual(tag.Name, name);
+      Assert.IsNotNull(tag.Id);
+      Assert.AreEqual(tag.Description, description);
+      Assert.AreEqual(tag.Color, new Color("#000000"));
+      Assert.IsNull(tag.TagParent);
+    }
+    [TestMethod]
+    [DataRow("Tag 1", "#000000")]
+    public void Constructor_ShouldCreateTag_WithColor(string name, string color)
+    {
+    Color color1 = new Color(color);
+      Tag tag = new Tag.TagBuilder()
+          .SetName(name)
+          .SetColor(color1)
+          .Build();
+
+      Assert.AreEqual(tag.Name, name);
+      Assert.IsNotNull(tag.Id);
+      Assert.IsNull(tag.Description);
+      Assert.AreEqual(tag.Color, color1);
+      Assert.IsNull(tag.TagParent);
+    }
+    [TestMethod]
+    [DataRow("Tag 1", "Tag 2")]
+    public void Constructor_ShouldCreateTag_WithParent(string name, string parentName)
+    {
+      Tag tagParent = new Tag.TagBuilder()
+          .SetName(parentName)
+          .Build();
+      Tag tag = new Tag.TagBuilder()
+          .SetName(name)
+          .SetTagParent(tagParent)
+          .Build();
+
+      Assert.AreEqual(tag.Name, name);
+      Assert.IsNotNull(tag.Id);
+      Assert.IsNull(tag.Description);
+      Assert.AreEqual(tag.Color, new Color("#000000"));
+      Assert.AreEqual(tag.TagParent, tagParent);
+    }
+    [TestMethod]
+    [DataRow("Tag 1", "Tag 2", "Description 1", "#000000")]
+    public void Constructor_ShouldCreateTag_WithAllProperties(string name, string parentName, string description, string color)
+    {
+    Color color1 = new Color(color);
+      Tag tagParent = new Tag.TagBuilder()
+          .SetName(parentName)
+          .Build();
+      Tag tag = new Tag.TagBuilder()
+          .SetName(name)
+          .SetDescription(description)
+          .SetColor(color1)
+          .SetTagParent(tagParent)
+          .Build();
+
+      Assert.AreEqual(tag.Name, name);
+      Assert.IsNotNull(tag.Id);
+      Assert.AreEqual(tag.Description, description);
+      Assert.AreEqual(tag.Color, color1);
+      Assert.AreEqual(tag.TagParent, tagParent);
     }
   }
 }
---
+++ TodoList.Domain.UnitTest/TagUpdate.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoList.Domain.Entities;
+using TodoList.Domain.ValueObjects;
+
+namespace TodoList.Domain.UnitTest
+{
+  [TestClass]
+  public class TagUpdate
+  {
+    [TestMethod]
+    [DataRow("Tag 1")]
+    public void UpdateName_ShouldUpdateTagName(string name)
+    {
+      Tag tag = new Tag.TagBuilder()
+          .SetName(name)
+          .Build();
+
+      string newName = "New Name";
+      tag.UpdateName(newName);
+
+      Assert.AreEqual(tag.Name, newName);
+    }
+    [TestMethod]
+    [DataRow("Description 1")]
+    public void UpdateDescription_ShouldUpdateTagDescription(string description)
+    {
+      Tag tag = new Tag.TagBuilder()
+          .SetName("Tag 1")
+          .Build();
+
+      tag.UpdateDescription(description);
+
+      Assert.AreEqual(tag.Description, description);
+    }
+    [TestMethod]
+    [DataRow("#000000")]
+    public void UpdateColor_ShouldUpdateTagColor(string color)
+    {
+      Color color1 = new Color(color);
+      Tag tag = new Tag.TagBuilder()
+          .SetName("Tag 1")
+          .Build();
+
+      tag.UpdateColor(color1);
+
+      Assert.AreEqual(tag.Color, color1);
+    }
+    [TestMethod]
+    [DataRow("Tag Parent")]
+    public void UpdateTagParent_ShouldUpdateTagParent(string parentName)
+    {
+      Tag tagParent = new Tag.TagBuilder()
+      .SetName(parentName)
+      .Build();
+
+      Tag tag = new Tag.TagBuilder()
+          .SetName("Tag 1")
+          .Build();
+
+      tag.UpdateTagParent(tagParent);
+
+      Assert.AreEqual(tag.TagParent, tagParent);
+    }
+    [TestMethod]
+    [DataRow("Tag Parent", "#000000", "Description 1", "New name")]
+    public void UpdateAllProperties_ShouldUpdateAllProperties(string parentName, string color, string description, string newname)
+    {
+      Tag tagParent = new Tag.TagBuilder()
+      .SetName(parentName)
+      .Build();
+      Color color1 = new Color(color);
+
+      Tag tag = new Tag.TagBuilder()
+          .SetName("Tag 1")
+          .Build();
+
+      tag.UpdateTagParent(tagParent);
+      tag.UpdateColor(color1);
+      tag.UpdateDescription(description);
+      tag.UpdateName(newname);
+
+      Assert.AreEqual(tag.TagParent, tagParent);
+      Assert.AreEqual(tag.Color, color1);
+      Assert.AreEqual(tag.Description, description);
+      Assert.AreEqual(tag.Name, newname);
+    }
+  }
+}
